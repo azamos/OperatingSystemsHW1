2e2cd8800c7969e110f3949fb4f27750a1b939d9
Amos Zoahr <azamoszohar@gmail.com>
2024-01-02 10:22:40 -0800

    fixed segmentation fault

--- try1.c
+++ try1.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
-
+#include <string.h>
 typedef struct {
 	char* name;
 	int grade;
@@ -11,7 +11,7 @@ int main(int argc, char* argv[]) {
 	char* dir_name = argc > 1? argv[1] : "";
 	sprintf(command, "ls %s -1 | wc -l",dir_name);//I have chosen to use sprintf with command as a buffer, instead of doing another malloc and strcpy
 	FILE *pipe = popen(command, "r");
-	
+
 	char buffer[128];//reasonable to assume less than a 128 digits number of students
 	//read the output of wc -l
 	if(fgets(buffer,sizeof(buffer),pipe)==NULL){
@@ -28,28 +28,33 @@ int main(int argc, char* argv[]) {
 
 	/*Now, to allocate space for the Students*/
 	Student** students = (Student**)malloc(sizeof(Student*)*num_of_students);
-	char command2[50];
+	for(int j=0;j<num_of_students;j++){
+		Student* stud_mem = (Student*)malloc(sizeof(Student));
+		students[j] = stud_mem;
+	}
+	char command2[100];
 	sprintf(command2,"ls %s -1",dir_name);
 	FILE *pipe2 = popen(command2,"r");
 	char names_buffer[50];
 	int i = 0;
-	while(fgets(names_buffer,sizeof(names_buffer),pipe2)!=NULL && i < num_of_students){
-		printf("\n Student name is %s",names_buffer);
+	while(fgets(names_buffer,sizeof(names_buffer),pipe2)!=NULL){
+		//printf("\n Student name is %s",names_buffer);
 		char* name_mem_aloc = (char*)malloc(sizeof(char)*(strlen(names_buffer)+1));
 		strcpy(name_mem_aloc,names_buffer);
-		students[i++]->name = name_mem_aloc;
+		students[i]->name = name_mem_aloc;
+		printf("\nstudents[%d]->name = %s",i,students[i]->name);
+		i++;
 	}
 	if(pclose(pipe2)==-1){
 		perror("pclose");
 		exit(EXIT_FAILURE);
 	}
 	/*freeing dynamic mem*/
-	for(i=0;i<num_of_students;i++){
-		printf("%dst student name: %s",i,students[i]);
-		free(students[i]->name);
+	for(int k =0 ;k<num_of_students;k++){
+		free(students[k]->name);
+		free(students[k]);
 	}
 	free(students);
-
 
 	return 0;
 }
