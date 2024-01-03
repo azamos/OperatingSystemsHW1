2393d1b9022a86a77a3e988f71231048159f7c90
Amos Zoahr <azamoszohar@gmail.com>
2024-01-02 18:32:47 -0800

    many problems were caused because of variable int i

--- try2.c
+++ try2.c
@@ -1,12 +1,16 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/wait.h>
 typedef struct {
 	char* name;
 	int grade;
 }Student;
 
 int main(int argc, char* argv[]) {
+	/*PART 1: extracting the 3 lines of data from the config file, whom path is specified in argv[1].*/
 	if (argc != 2) {
 		printf("Usage: %s <file_path>\n", argv[0]);
 		return 1; 
@@ -19,7 +23,7 @@ int main(int argc, char* argv[]) {
 	}
 	char* lines[3];
 	int counter = 0;
-	char config_buffer[256]; 
+	char config_buffer[51]; //changed to 50
 	while (fgets(config_buffer, sizeof(config_buffer), config_file) != NULL) {
 		//printf("%s", config_buffer); 
 		int n = strlen(config_buffer);
@@ -39,7 +43,7 @@ int main(int argc, char* argv[]) {
 		printf("DIFFERENT STRINGS");
 	}*/
 
-	/*PART 2*/
+	/*PART 2: extracting the amount of subfolders and their names, to prepare associated Student instances.*/
 
 
 	char command[384];
@@ -59,7 +63,7 @@ int main(int argc, char* argv[]) {
 	}
 	//printf("buffer is:\n %s\n",buffer);
 	int num_of_students = atoi(buffer);
-	printf("\nNumber of students: %d\n",num_of_students);
+	//printf("\nNumber of students: %d\n",num_of_students);
 	if(pclose(pipe)==-1){
 		perror("pclose");
 		exit(EXIT_FAILURE);
@@ -75,25 +79,45 @@ int main(int argc, char* argv[]) {
 	sprintf(command2,"ls %s -1",dir_name);
 	FILE *pipe2 = popen(command2,"r");
 	char names_buffer[50];
-	int i = 0;
+	int index = 0;
 	while(fgets(names_buffer,sizeof(names_buffer),pipe2)!=NULL){
 		//printf("\n Student name is %s",names_buffer);
 		char* name_mem_aloc = (char*)malloc(sizeof(char)*(strlen(names_buffer)+1));
+		//printf("VALIDATING NAME WITHOUT WS: %ld",strlen(names_buffer));
 		strcpy(name_mem_aloc,names_buffer);
-		students[i]->name = name_mem_aloc;
-		printf("\nstudents[%d]->name = %s",i,students[i]->name);
-		i++;
+		name_mem_aloc[strlen(names_buffer)]='\0';//TODO: remove if not fix
+		students[index]->name = name_mem_aloc;
+		//printf("\nstudents[%d]->name = %s,student_len = %ld",i,students[i]->name,strlen(students[i]->name));
+		index++;
 	}
 	if(pclose(pipe2)==-1){
 		perror("pclose");
 		exit(EXIT_FAILURE);
 	}
-	/*freeing dynamic mem*/
+
+	/*PART 3: forking and executing the main of each student with the input provided in lines[1].*/
+
+	for(int j=0;j<num_of_students;j++){
+		printf("students[j=%d] name is %s\n",j,students[j]->name);
+		int path_to_subfolders_len = strlen(lines[0]);
+		//printf("path_len = %d",path_to_subfolders_len);
+		int subfolder_name_len = strlen(students[j]->name);
+		//printf("subfolder_namne_len = %ld",subfolder_name_len);
+		char* suffix = "/main.exe";
+		int suffix_len = strlen(suffix);
+		int total_len = path_to_subfolders_len+subfolder_name_len+suffix_len+1;
+		printf("total len = %d\n",total_len);
+	}
+
+	/*FINAL PART: freeing dynamicaLLY allocated mem.*/
 	for(int k =0 ;k<num_of_students;k++){
 		free(students[k]->name);
 		free(students[k]);
 	}
 	free(students);
+	for(int j=0;j<3;j++){
+		free(lines[j]);
+	}
 
 	return 0;
 }
